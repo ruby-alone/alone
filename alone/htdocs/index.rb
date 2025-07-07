028db3d00a6e601493c370db819af3487eb837f1
HirohitoHigashi <higashi@s-itoc.jp>
2025-07-07 10:15:36 +0900

    change require to require_relative.

--- alone/htdocs/index.rb
+++ alone/htdocs/index.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
-# -*- coding: utf-8 -*-
+#
 # alone : application framework for embedded systems.
-#               Copyright (c) 2009-2010 Inas Co Ltd. All Rights Reserved.
 #
 # This file is destributed under BSD License. Please read the LICENSE file.
 #
 # エントリーポイント
 #
 # 導入する環境にあわせて、shebang（一行目）とal_configのパスを書換える。
-# CGIそのものが動作しているか確認するには、以下の行を有功にして確認できる。
-#  puts "Content-Type: text/plain\r\n\r\nIt works!"
+# CGIそのものが動作しているか確認するには、以下の行を有功にし、ブラウザ
+# 画面に It works! と表示されるかを確認する。
+#  puts "Content-Type: text/plain\r\n\r\nIt works!"; exit
 
-require '../al_config'
+require_relative '../al_config'
 require 'al_controller'
